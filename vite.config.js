b95f3b7fa6944d600cfe51be4465476df16e5cbf
Harsh Kumar <harsh10oct02@gmail.com>
2025-08-17 01:10:54 +0530

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // your site will live at https://harshkumar02.github.io/portofolio-harsh/
+  base: '/portofolio-harsh/',
 })
