3523624662020ca24ef88ce634d73d677640e305
Harsh Kumar <harsh10oct02@gmail.com>
2025-08-17 01:47:12 +0530

    base change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // your site will live at https://harshkumar02.github.io/portofolio-harsh/
-  base: '/portofolio-harsh/',
+  base: '/Portofolio_main/',
 })
